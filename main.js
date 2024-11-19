0e4095e1abb7c4fd3ef6ea1ae3a5561a945f8bce
Nataly Zernova <nz_18@mail.ru>
2024-11-19 18:47:11 +0300

    perf: add normalize.css file

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
+import "normalize.css";
 import "./style.css";
-import javascriptLogo from "./javascript.svg";
-import viteLogo from "/vite.svg";
 import { setupCounter } from "./counter.js";
 
 const mixed = [
